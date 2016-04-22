697d4bf11ce8897daa31a838859ae2f86bf90bf3
CasualDutchman <casual.dutchman@live.nl>
2016-04-22 12:44:27 +0200

    Added files via upload

---
+++ CustomScenery/Shop/Decorator/ColorDecorator.cs
@@ -0,0 +1,42 @@
+﻿using CustomShops;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom_Scenery.CustomScenery.Shop.Decorator
+{
+    class ColorDecorator : IDecorator
+    {
+        bool _color;
+
+        public ColorDecorator(bool color)
+        {
+            _color = color;
+        }
+
+        public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
+        {
+            foreach (Material material in AssetManager.Instance.objectMaterials)
+            {
+                if (material.name == "CustomColorsDiffuse")
+                {
+                    SetMaterial(go, material);
+
+                    break;
+                }
+            }
+        }
+
+        private void SetMaterial(GameObject go, Material material)
+        {
+            // Go through all child objects and recolor		
+            Renderer[] renderCollection;
+            renderCollection = go.GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer render in renderCollection)
+            {
+                render.sharedMaterial = material;
+            }
+        }
+    }
+}
---
+++ CustomScenery/Shop/Decorator/NameDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace CustomShops
+{
+	public class NameDecorator
+	{
+        private string _name;
+        private string _display = "null";
+
+        public NameDecorator(string name)
+        {
+            _name = name;
+            _display = name;
+        }
+        public NameDecorator(string name, string display)
+        {
+            _name = name;
+            _display = display;
+        }
+
+        public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
+        {
+            go.GetComponent<BuildableObject>().setDisplayName(_display);
+            go.name = _name;
+        }
+    }
+}
+

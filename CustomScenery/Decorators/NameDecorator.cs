de121887018888911c9ffcbd6be69080f53469c8
CasualDutchman <casual.dutchman@live.nl>
2016-04-22 12:40:58 +0200

    Added files via upload

--- CustomScenery/Decorators/NameDecorator.cs
+++ CustomScenery/Decorators/NameDecorator.cs
@@ -6,15 +6,22 @@ namespace Custom_Scenery.Decorators
     class NameDecorator : IDecorator
     {
         private string _name;
+        private string _display = "null";
 
         public NameDecorator(string name)
         {
             _name = name;
+            _display = name;
+        }
+        public NameDecorator(string name, string display)
+        {
+            _name = name;
+            _display = display;
         }
 
         public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
         {
-            go.GetComponent<BuildableObject>().setDisplayName(_name);
+            go.GetComponent<BuildableObject>().setDisplayName(_display);
             go.name = _name;
         }
     }
---
+++ CustomScenery/Decorators/WindDecorator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom_Scenery.Decorators
+{
+    class WindDecorator : IDecorator
+    {
+        private bool _wind;
+        private Wind_Check_Horizontal hor;
+
+        public WindDecorator(bool wind){ _wind = wind; }
+
+        public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
+        {
+            if (_wind)
+                {
+                    go.AddComponent<Wind_Check>();
+                }
+                else
+                {
+                    hor = go.AddComponent<Wind_Check_Horizontal>();
+                    if (options.ContainsKey("mass"))
+                    {
+                        hor.strength = (float)((double)options["mass"]);
+                    }
+                    else
+                    {
+                        hor.strength = 1.0f;
+                    }
+                }
+        }
+    }
+}
